6dcfe911bd2008dbdba4302615ccf3f3f8571dde
JaimeAndresLopez <james_2005_scott@hotmail.coml>
2021-05-04 17:17:54 -0500

    Condicionales 04-05-2021

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+ 
+    $num1 = (int) 5;
+    $num2 = (int) 10;
+
+    //var_dump($num1 > $num2 || $num1 < $num2); Ejercicios para practicar orden y saber q se obtiene True o False
+
+     $num1 = (int) 2;
+//     if($num1 > 0 && $num1 <= 5){     
+//         if($num1==1){
+//             echo <<<NumeroEsUno
+//             El numero $num1 es igual un 1
+// NumeroEsUno;
+//         }elseif($num1==2){
+//             echo <<<NumeroEsUno
+//             El numero $num1 es igual a 2
+// NumeroEsUno;
+//         }else{
+//             echo <<<NumeroEsUno
+//             El numero $num1 es mayor o igual a 0
+// NumeroEsUno;
+//         }
+
+//     }elseif($num1 == 0){
+//         echo <<<NumeroEsIgual0
+//         El numero $num1 es igual a 0
+// NumeroEsIgual0;
+//     }elseif($num1 < 0 && $num1 >= -5){
+//         echo <<<NumeroEsnegativo
+//         El numero $num1 es es negativo
+// NumeroEsnegativo;
+//     }else{
+//         echo <<<ProbabilidadNoExistente
+//         El numero $num1 no tiene una condicion o una probabilidad
+// ProbabilidadNoExistente;
+//     }
+
+?>
